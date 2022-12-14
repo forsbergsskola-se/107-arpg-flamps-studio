e6663c668adbd7c7f23b3e9e6baff2d24ddffe2a
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-12-14 06:49:05 +0100

    GameEngine to test health system
    set health to 100, A damages health by 10, H heals health by 10 clamped between 0 & max

--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameEngine : MonoBehaviour
 {
+    HealthSystem healthSystem;
     void Start()
     {
-        HealthSystem healthSystem = new HealthSystem(100);
+        this.healthSystem = new HealthSystem(100);
         
         Debug.Log("Health; " +healthSystem.GetHealth());
     }
     
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            healthSystem.Damage(10);
+            Debug.Log("Damaged: " + healthSystem.GetHealth());
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            healthSystem.Heal(10);
+            Debug.Log("Healed: " + healthSystem.GetHealth());
+        }
     }
 }
