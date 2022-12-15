1eae4fd0614c4f22be05c5791ff76a7e984f5421
Lawrence <lawrencealejo@hotmail.se>
2022-12-15 22:02:51 +0100

    ready for pull request
    fireball, manareg, cd, damagetaken.

---
+++ Assets/Abilities/Scripts/PlayerMagicSystem.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+//attach to player
+public class PlayerMagicSystem : MonoBehaviour
+{
+    [SerializeField] private Spell spellToCast;
+    
+    [SerializeField] private float maxMana = 100f;
+    [SerializeField] private float currentMana;
+    [SerializeField] private float manaRechargeRate = 2f;
+    [SerializeField] private float timeToWaitForRecharge = 1f;
+    private float _currentManaRechageTimer;
+    [SerializeField] private float timeBetweenCast = 2f;
+    private float _currentCastTimer;
+        
+    [SerializeField] private Transform castPoint;
+    
+    private bool _castingMagic = false;
+
+    private void Awake()
+    {
+        currentMana = maxMana;
+    }
+
+    private void Update()
+    {
+        bool isSpellCastingHeldDown = Input.GetKeyDown(KeyCode.Alpha1);
+        bool hasEnoughMana = currentMana - spellToCast.spellToCast.manaCost >= 0f;   
+        
+        if (!_castingMagic && isSpellCastingHeldDown && hasEnoughMana)
+        {
+            _castingMagic = true;
+            currentMana -= spellToCast.spellToCast.manaCost;
+            _currentCastTimer = 0;
+            _currentManaRechageTimer = 0;
+            CastSpell();
+        }
+
+        if (_castingMagic)
+        {
+            _currentCastTimer += Time.deltaTime;
+            if (_currentCastTimer > timeBetweenCast) _castingMagic = false;
+        }
+
+        if (currentMana < maxMana && !_castingMagic && !isSpellCastingHeldDown)
+        {
+            _currentManaRechageTimer += Time.deltaTime;
+
+            if (_currentManaRechageTimer > timeToWaitForRecharge)
+            {
+                currentMana += manaRechargeRate * Time.deltaTime;
+                if (currentMana > maxMana) currentMana = maxMana;
+            }
+        }
+    }
+
+    private void CastSpell()
+    {
+        Instantiate(spellToCast, castPoint.position, castPoint.rotation);
+    }
+        
+}
