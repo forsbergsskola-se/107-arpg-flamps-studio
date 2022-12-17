1626b8399e8389a3ab3cebf7dd02b5bd8452fdc4
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-12-17 04:44:41 +0100

    Reformat and applied

--- Assets/Scripts/CameraRig.cs
+++ Assets/Scripts/CameraRig.cs
@@ -1,41 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRig : MonoBehaviour
 {
     public float moveSpeed;
     public GameObject target;
-    
-    private Transform rigTransform;
 
-    void Start()
-    {
-    rigTransform = this.transform.parent;
-    }
-    void FixedUpdate ()
-    {
-        if(target == null)
-        {
-            return;
-        }
+    private Transform _rigTransform;
 
-    rigTransform.position = Vector3.Lerp(rigTransform.position, target.transform.position, Time.deltaTime * moveSpeed);
-    }
-/*Basic Camera Zoom with Scrollwheel
-    void Update ()
+    private void Start()
     {
-        if (Input.GetAxis ("Mouse ScrollWheel") > 0)
-        {
-//            GetComponent<Camera>().fieldOfView--;
-            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y-0.6f, transform.position.z+0.2f);
-            transform.Rotate(-2, 0, 0);
-        }
-        if (Input.GetAxis ("Mouse ScrollWheel") < 0)
-        {
-//            GetComponent<Camera>().fieldOfView++;
-            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y+0.6f, transform.position.z-0.2f);
-            transform.Rotate(2, 0, 0);
-        }
-    }*/
+        _rigTransform = transform.parent;
+    }
+
+    private void FixedUpdate()
+    {
+        if (target == null) return;
+
+        _rigTransform.position =
+            Vector3.Lerp(_rigTransform.position, target.transform.position, Time.deltaTime * moveSpeed);
+    }
 }
--- Assets/Scripts/MoveToClickPoint.cs
+++ Assets/Scripts/MoveToClickPoint.cs
@@ -1,24 +1,25 @@
 // MoveToClickPoint.cs
+
 using UnityEngine;
 using UnityEngine.AI;
-    
+
 public class MoveToClickPoint : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent player;
     [SerializeField] private Animator anim;
     [SerializeField] private float distanceToTarget = 2f;
-    
+
     private Transform _target;
     private Camera _mainCam;
     private static readonly int IsWalking = Animator.StringToHash("isWalking");
 
-    void Awake() 
+    private void Awake()
     {
         _mainCam = Camera.main;
         player = GetComponent<NavMeshAgent>();
     }
-        
-    void Update()
+
+    private void Update()
     {
         anim.SetFloat(IsWalking, player.velocity.magnitude / player.speed);
 
@@ -27,25 +28,25 @@ public class MoveToClickPoint : MonoBehaviour
         if (_target != null)
         {
             var between = _target.position - transform.position;
-            
-            if(between.magnitude <= distanceToTarget)
+
+            if (between.magnitude <= distanceToTarget)
                 return;
-            
+
             player.SetDestination(_target.position);
         }
     }
 
     private void GetDestination()
     {
-        if (!Input.GetMouseButtonDown(1)) 
+        if (!Input.GetMouseButtonDown(1))
             return;
-        
-        if (!Physics.Raycast(_mainCam.ScreenPointToRay(Input.mousePosition), out var hit)) 
+
+        if (!Physics.Raycast(_mainCam.ScreenPointToRay(Input.mousePosition), out var hit))
             return;
-            
-        if(hit.collider.CompareTag("GoTo")) // This means we hit a follow Target
+
+        if (hit.collider.CompareTag("GoTo")) // This means we hit a follow Target
         {
-            Debug.Log("Target assigned : " + hit.transform.gameObject.name); 
+            Debug.Log("Target assigned : " + hit.transform.gameObject.name);
             _target = hit.transform;
         }
         else
