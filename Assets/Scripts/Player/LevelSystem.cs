b247f672e7d0655510112f94eb13d691268bc5cf
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-03 08:45:35 +0100

    Replaced Test Enemy models with shapes

--- Assets/Scripts/Player/LevelSystem.cs
+++ Assets/Scripts/Player/LevelSystem.cs
@@ -1,60 +1,63 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class LevelSystem : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private int level;
-    [SerializeField] private int xpPerLevel;
-    private int _curXp;
+    public class LevelSystem : MonoBehaviour
+    {
+        [SerializeField] private int level;
+        [SerializeField] private int xpPerLevel;
+        private int _curXp;
     
-    public UnityEvent<int, int> experienceUpdated;
-    public UnityEvent<int> leveledUp;
-    public HealthSystem healthSystem;
-    public ManaSystem manaSystem;
-    public int healthPerLevel;
-    public int manaPerLevel;
+        public UnityEvent<int, int> experienceUpdated;
+        public UnityEvent<int> leveledUp;
+        public HealthSystem healthSystem;
+        public ManaSystem manaSystem;
+        public int healthPerLevel;
+        public int manaPerLevel;
 
 
         
-    public int CurrentXp
-    {
-        get => _curXp; 
-        private set
+        public int CurrentXp
         {
-            _curXp = Mathf.Clamp(value, 0, int.MaxValue);
-            experienceUpdated?.Invoke(_curXp, XpPerLevel);
+            get => _curXp; 
+            private set
+            {
+                _curXp = Mathf.Clamp(value, 0, int.MaxValue);
+                experienceUpdated?.Invoke(_curXp, XpPerLevel);
+            }
         }
-    }
 
-    public int CurrentLevel
-    {
-        get => level;
-        private set
+        public int CurrentLevel
         {
-            level = Mathf.Clamp(value, 0, int.MaxValue);
-            leveledUp?.Invoke(level);
+            get => level;
+            private set
+            {
+                level = Mathf.Clamp(value, 0, int.MaxValue);
+                leveledUp?.Invoke(level);
+            }
         }
-    }
 
-    public int XpPerLevel => xpPerLevel;
+        public int XpPerLevel => xpPerLevel;
 
-    public void AddXp(int amount)
-    {
-        CurrentXp += amount;
-        if (CurrentXp >= XpPerLevel)
+        public void AddXp(int amount)
         {
-            LevelUp();
+            CurrentXp += amount;
+            if (CurrentXp >= XpPerLevel)
+            {
+                LevelUp();
+            }
         }
-    }
 
-    private void LevelUp()
-    {
-        while (CurrentXp >= XpPerLevel)
+        private void LevelUp()
         {
-            CurrentLevel++;
-            CurrentXp -= XpPerLevel;
-            //healthSystem.IncreaseMaxHealth(healthPerLevel);
-            //manaSystem.IncreaseMaxMana(manaPerLevel);
+            while (CurrentXp >= XpPerLevel)
+            {
+                CurrentLevel++;
+                CurrentXp -= XpPerLevel;
+                //healthSystem.IncreaseMaxHealth(healthPerLevel);
+                //manaSystem.IncreaseMaxMana(manaPerLevel);
+            }
         }
     }
 }
