ae2fdb591242a7b8feacf27c3f85af0af0054122
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-04 15:46:21 +0100

    Add waypoint marker
    to indicate where mouse was clicked and player is moving to

--- Assets/Scripts/Player/MoveToClickPoint.cs
+++ Assets/Scripts/Player/MoveToClickPoint.cs
@@ -1,4 +1,3 @@
-// MoveToClickPoint.cs
 using UnityEngine;
 using UnityEngine.AI;
     
@@ -7,9 +6,12 @@ public class MoveToClickPoint : MonoBehaviour
     [SerializeField] private NavMeshAgent player;
     [SerializeField] private Animator anim;
     [SerializeField] private float distanceToTarget = 2f;
+    [SerializeField] private float threshold = 1f;
+    [SerializeField] private GameObject markerPrefab; // Add this field
     
     private Transform target;
     private Camera mainCam;
+    private GameObject marker; // Add this field
         
     void Awake() 
     {
@@ -32,7 +34,19 @@ public class MoveToClickPoint : MonoBehaviour
             
             player.SetDestination(target.position);
         }
+
+        // Check if the distance between the player and the marker is less than the threshold
+        if (marker != null)
+        {
+            float distance = Vector3.Distance(transform.position, marker.transform.position);
+            if (distance < threshold)
+            {
+                // Destroy the marker
+                Destroy(marker);
+            }
+        }
     }
+
 
     private void GetDestination()
     {
@@ -51,6 +65,17 @@ public class MoveToClickPoint : MonoBehaviour
         {
             target = null;
             player.SetDestination(hit.point);
+            
+            // Destroy the previous marker
+            if (marker != null)
+            {
+                Destroy(marker);
+            }
+            
+            // Instantiate the marker prefab at the destination point, 1 unit above the ground
+            marker = Instantiate(markerPrefab, hit.point + Vector3.up * 0.5f, Quaternion.identity);
+
         }
     }
 }
+
