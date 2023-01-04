b3bc22aae4f734e17f8b481fb145198f87290811
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-04 15:50:54 +0100

    Update MoveToClickPoint.cs

--- Assets/Scripts/Player/MoveToClickPoint.cs
+++ Assets/Scripts/Player/MoveToClickPoint.cs
@@ -1,80 +1,83 @@
 using UnityEngine;
 using UnityEngine.AI;
-    
-public class MoveToClickPoint : MonoBehaviour
+
+namespace Player
 {
-    [SerializeField] private NavMeshAgent player;
-    [SerializeField] private Animator anim;
-    [SerializeField] private float distanceToTarget = 2f;
-    [SerializeField] private float threshold = 1f;
-    [SerializeField] private GameObject markerPrefab; // Add this field
+    public class MoveToClickPoint : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent player;
+        [SerializeField] private Animator anim;
+        [SerializeField] private float distanceToTarget = 2f;
+        [SerializeField] private float threshold = 1f;
+        [SerializeField] private GameObject markerPrefab; // Add this field
     
-    private Transform target;
-    private Camera mainCam;
-    private GameObject marker; // Add this field
+        private Transform _target;
+        private Camera _mainCam;
+        private GameObject _marker; // Add this field
         
-    void Awake() 
-    {
-        mainCam = Camera.main;
-        player = GetComponent<NavMeshAgent>();
-    }
-        
-    void Update()
-    {
-        anim.SetFloat("isWalking", player.velocity.magnitude/player.speed);
-
-        GetDestination();
-
-        if (target != null)
+        void Awake() 
         {
-            var between = target.position - transform.position;
+            _mainCam = Camera.main;
+            player = GetComponent<NavMeshAgent>();
+        }
+        
+        void Update()
+        {
+            anim.SetFloat("isWalking", player.velocity.magnitude/player.speed);
+
+            GetDestination();
+
+            if (_target != null)
+            {
+                var between = _target.position - transform.position;
             
-            if(between.magnitude <= distanceToTarget)
+                if(between.magnitude <= distanceToTarget)
+                    return;
+            
+                player.SetDestination(_target.position);
+            }
+
+            // Check if the distance between the player and the marker is less than the threshold
+            if (_marker != null)
+            {
+                float distance = Vector3.Distance(transform.position, _marker.transform.position);
+                if (distance < threshold)
+                {
+                    // Destroy the marker
+                    Destroy(_marker);
+                }
+            }
+        }
+
+
+        private void GetDestination()
+        {
+            if (!Input.GetMouseButtonDown(1)) 
+                return;
+        
+            if (!Physics.Raycast(_mainCam.ScreenPointToRay(Input.mousePosition), out var hit)) 
                 return;
             
-            player.SetDestination(target.position);
-        }
-
-        // Check if the distance between the player and the marker is less than the threshold
-        if (marker != null)
-        {
-            float distance = Vector3.Distance(transform.position, marker.transform.position);
-            if (distance < threshold)
+            if(hit.collider.CompareTag("GoTo")) //This means we hit a follow Target
             {
-                // Destroy the marker
-                Destroy(marker);
+                Debug.Log("Target assigned : "+hit.transform.gameObject.name); 
+                _target = hit.transform;
             }
-        }
-    }
-
-
-    private void GetDestination()
-    {
-        if (!Input.GetMouseButtonDown(1)) 
-            return;
-        
-        if (!Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out var hit)) 
-            return;
-            
-        if(hit.collider.CompareTag("GoTo")) //This means we hit a follow Target
-        {
-            Debug.Log("Target assigned : "+hit.transform.gameObject.name); 
-            target = hit.transform;
-        }
-        else
-        {
-            target = null;
-            player.SetDestination(hit.point);
-            
-            // Destroy the previous marker
-            if (marker != null)
+            else
             {
-                Destroy(marker);
-            }
+                _target = null;
+                player.SetDestination(hit.point);
             
-            // Instantiate the marker prefab at the destination point, 1 unit above the ground
-            marker = Instantiate(markerPrefab, hit.point + Vector3.up * 0.5f, Quaternion.identity);
+                // Destroy the previous marker
+                if (_marker != null)
+                {
+                    Destroy(_marker);
+                }
+            
+                // Instantiate the marker prefab at the destination point, 1 unit above the ground
+                _marker = Instantiate(markerPrefab, hit.point + Vector3.up * 0.5f, Quaternion.identity);
 
+            }
         }
     }
 }
