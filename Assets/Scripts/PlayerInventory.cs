5dde3f13dce1aa497c755c1455c08b30c9a0fb0c
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-09 17:39:50 +0100

    rename Player to PlayerInventory.cs

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerInventory : MonoBehaviour
 {
     public InventoryObject inventory;
     // Start is called before the first frame update
