3c7d925202ecf1054f6e405fd2fb7fe4be126a10
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-11 19:42:32 +0100

    Update WeaponAttach.cs

--- Assets/Scripts/WeaponAttach.cs
+++ Assets/Scripts/WeaponAttach.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class WeaponAttach : MonoBehaviour
 {
+    public GameObject Player;
     public Sword sword;
-    public Axe axe;
+    //public Axe axe;
     public GameObject swordWeapon; //the sword prefab (your choice)
-    public GameObject axeWeapon; // the axe prefab (your choice)
+    //public GameObject axeWeapon; // the axe prefab (your choice)
     public GameObject weaponHolder; //empty object attached to player arm
     public EquippedWeapon currentWeapon;
 
@@ -15,8 +16,9 @@ public class WeaponAttach : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1))
-            axeWeapon.transform.SetParent(weaponHolder.transform); //attaches weapon of choice to weaponHolder.
+        var distance = Player.transform.position - transform.position;
+        if (Input.GetKeyDown(KeyCode.O) && distance.magnitude < 2.5f) 
+            swordWeapon.transform.SetParent(weaponHolder.transform); //attaches weapon of choice to weaponHolder.
         //(using axe for testing purposes only)
 
         //code below changes equipped weapon scriptable object to match weapon type.
@@ -25,10 +27,10 @@ public class WeaponAttach : MonoBehaviour
             currentWeapon.damage = sword.damage;
             currentWeapon.attackCooldown = sword.attackCooldown;
         }
-        else if (axeWeapon.transform.IsChildOf(weaponHolder.transform))
+        /*else if (axeWeapon.transform.IsChildOf(weaponHolder.transform))
         {
             currentWeapon.damage = axe.damage;
             currentWeapon.attackCooldown = axe.attackCooldown;
-        }
+        }*/
     }
 }
