f43b1b1c65afb4da9ecf11acee77763ca53528d8
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-01-11 15:04:20 +0100

    Update LevelSystem.cs

--- Assets/Scripts/Player/LevelSystem.cs
+++ Assets/Scripts/Player/LevelSystem.cs
@@ -5,27 +5,22 @@ namespace Player
 {
     public class LevelSystem : MonoBehaviour
     {
-        [SerializeField] public  int level;
+        [SerializeField] public int level;
         [SerializeField] private int xpPerLevel;
-        public static int _curXp;
+        private int _curXp;
         public GameObject player;
         public UnityEvent<int, int> experienceUpdated;
         public UnityEvent<int> leveledUp;
+        public GameObject levelUpEffect;
         public HealthSystem healthSystem;
         public ManaSystem manaSystem;
         public int healthPerLevel;
         public int manaPerLevel;
 
-
-        
         public int CurrentXp
         {
             get => _curXp;
-            set
-            {
-                _curXp = Mathf.Clamp(value, 0, int.MaxValue);
-                experienceUpdated?.Invoke(_curXp, XpPerLevel);
-            }
+            set => _curXp = value;
         }
 
         public int CurrentLevel
@@ -42,19 +37,29 @@ namespace Player
 
         public void AddXp(int amount)
         {
-            CurrentXp += amount;
-            if (CurrentXp >= XpPerLevel)
+            _curXp += amount;
+            experienceUpdated?.Invoke(_curXp, XpPerLevel);
+            if (_curXp >= XpPerLevel)
             {
                 LevelUp();
             }
         }
-
+        public void SetXp(int amount)
+        {
+            _curXp = amount;
+            experienceUpdated?.Invoke(_curXp, XpPerLevel);
+            if (_curXp >= XpPerLevel)
+            {
+                LevelUp();
+            }
+        }
         private void LevelUp()
         {
-            while (CurrentXp >= XpPerLevel)
+            while (_curXp >= XpPerLevel)
             {
                 CurrentLevel++;
-                CurrentXp -= XpPerLevel;
+                _curXp -= XpPerLevel;
+                Instantiate(levelUpEffect, transform.position, Quaternion.identity);
                 //healthSystem.IncreaseMaxHealth(healthPerLevel);
                 //manaSystem.IncreaseMaxMana(manaPerLevel);
             }
