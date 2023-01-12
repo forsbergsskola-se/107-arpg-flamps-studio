99cda99a8a233b152815972ba860994deed0c2b8
FelixAtForsberg <113030003+FelixAtForsberg@users.noreply.github.com>
2023-01-12 17:55:00 +0100

    New stat HP/XP runtime storage component script + UI scene

---
+++ Assets/Scripts/Player/PlayerBars_HP_XP.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBars_HP_XP : MonoBehaviour
+{
+    public RectTransform XPPanelReference;
+    public TextMesh HPTextReference;
+    public RectTransform HPPanelReference;
+
+    public int healthMax;
+    private int _healthPercentage;
+    private float _healthPercentageNorm;
+
+    private float hpPanelMaxWidth;
+    private float xpPanelMaxWidth;
+    
+    private int _healthCur;
+    public int HealthCur
+    {
+        get => _healthCur;
+        set
+        {
+            _healthPercentageNorm = (float)value / healthMax;
+            _healthPercentage = (int)_healthPercentageNorm * 100;
+
+            Rect newHpBarRect = HPPanelReference.rect;
+            // newHpBarRect.width = hpPanelMaxWidth * _healthPercentageNorm;
+            
+            // HPPanelReference.rect.width = newHpBarRect;
+            HPTextReference.text = $"HP: {_healthPercentage}%";
+            _healthCur = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (HPTextReference is { })
+            throw new NullReferenceException("HP Text Reference is required");
+        
+        if (HPPanelReference is { })
+            throw new NullReferenceException("HP Panel Reference is required");
+        
+        if (XPPanelReference is { })
+            throw new NullReferenceException("XP Panel Reference is required");
+
+
+        hpPanelMaxWidth = HPPanelReference.rect.width;
+        xpPanelMaxWidth = XPPanelReference.rect.width;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
