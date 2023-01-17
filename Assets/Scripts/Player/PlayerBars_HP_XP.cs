2ad01373d2ea2a8ce4f3becb615361eafe72e9a9
FelixAtForsberg <113030003+FelixAtForsberg@users.noreply.github.com>
2023-01-17 12:32:07 +0100

    Player HUD + HP Script

--- Assets/Scripts/Player/PlayerBars_HP_XP.cs
+++ Assets/Scripts/Player/PlayerBars_HP_XP.cs
@@ -2,56 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBars_HP_XP : MonoBehaviour
 {
-    public RectTransform XPPanelReference;
-    public TextMesh HPTextReference;
-    public RectTransform HPPanelReference;
+    // public RectTransform xpPanelReference;
+    public GameObject XPPanelGameObject;
+    public GameObject HPTextGameObject;
+    public GameObject HPPanelGameObject;
 
-    public int healthMax = 100;
-    private int _healthPercentage;
-    private float _healthPercentageNorm;
-
-    private float hpPanelMaxWidth;
-    private float xpPanelMaxWidth;
+    private TextMesh _hpText;
+    private RectTransform _hpRect;
+    private RectTransform _xpRect;
     
+    public int healthMax = 100;
+    private float _healthPercentageFract;
+    private float _healthPercentage;
+
     private int _healthCur;
     public int HealthCur
     {
         get => _healthCur;
         set
         {
-            _healthPercentageNorm = (float)value / healthMax;
-            _healthPercentage = (int)_healthPercentageNorm * 100;
-
-            Rect newHpBarRect = HPPanelReference.rect;
-            // newHpBarRect.width = hpPanelMaxWidth * _healthPercentageNorm;
-            
-            // HPPanelReference.rect.width = newHpBarRect;
-            HPTextReference.text = $"HP: {_healthPercentage}%";
+            OnHealthChange(_healthCur, value);
             _healthCur = value;
         }
     }
+    
+    private void OnHealthChange(int oldHealth, int newHealth) 
+    {
+        _healthPercentageFract = (float)newHealth / healthMax;
+            _healthPercentage = (int)_healthPercentageFract * 100;
 
+            ScaleRect(_hpRect, _healthPercentageFract);
+            _hpText.text = $"HP: {_healthPercentage}%";
+    }
+
+    private void ScaleRect(RectTransform rectTransform, int valCur, int valMax)
+    {
+        float percentageFract = Mathf.Clamp( (float)valCur / valMax, 0, 1 );
+        rectTransform.localScale = new Vector3(1, percentageFract, 1); // cannot modify struct field directly
+    }   
+    
+    private void ScaleRect(RectTransform rectTransform, float scaleY)
+    {
+        rectTransform.localScale = new Vector3(1, scaleY, 1); // cannot modify struct field directly
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        if (HPTextReference is { })
-            throw new NullReferenceException("HP Text Reference is required");
-        
-        if (HPPanelReference is { })
-            throw new NullReferenceException("HP Panel Reference is required");
-        
-        if (XPPanelReference is { })
-            throw new NullReferenceException("XP Panel Reference is required");
+        _healthCur = healthMax;
+        _hpText = HPTextGameObject.GetComponent<TextMesh>();
+        _hpRect = HPTextGameObject.GetComponent<RectTransform>();
+        _xpRect = HPTextGameObject.GetComponent<RectTransform>();
 
-        
-        hpPanelMaxWidth = HPPanelReference.rect.width;
-        xpPanelMaxWidth = XPPanelReference.rect.width;
-        
-        // has to be set after other things ahve been initialized
-        HealthCur = healthMax;
     }
 
     // Update is called once per frame
